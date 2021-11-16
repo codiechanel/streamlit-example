097a3a4620e640bc01c94a846d27a7f1215227f3
codie <codiechanel@gmail.com>
2021-11-16 20:17:19 +0800

    kl

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,10 @@
 import streamlit as st
 import plotly.express as px
-fig = px.histogram([1,2,2,3,3,3,4,4,5])
+import pandas as pd
+# fig = px.histogram([1, 2, 2, 3, 3, 3, 4, 4, 5])
 
+df = px.data.gapminder().query("year==2007")
+# df.head()
+
+fig = px.histogram(df, x="lifeExp", color="continent", hover_name="country")
 st.plotly_chart(fig)
